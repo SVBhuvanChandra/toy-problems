715b89d975b24fea2ef880b2829202ea4981f589
SVBhuvanChandra <s.v.bhuvanchandra@gmail.com>
2020-04-14 17:03:49 +0530

    decided 3 empty methods

--- lru_method.py
+++ lru_method.py
@@ -3,5 +3,14 @@ class LRUcache:
         self.cacheSize = cacheSize
         self.cache = []
 
+    def put(self, additem):
+        pass
+
+    def get(self, getitem):
+        pass
+
+    def get_cache(self):
+        pass
+
 if __name__ == "__main__":
     LRUcache(input("enter size"))
